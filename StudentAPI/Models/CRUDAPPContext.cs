bb49030d89c79347b5437190b2afa72ebbb0a71a
varunak <varunakars@cybage.com>
2022-11-18 17:09:16 +0530

    File Addedd

---
+++ StudentAPI/Models/CRUDAPPContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace StudentAPI.Models
+{
+    public partial class CRUDAPPContext : DbContext
+    {
+        public CRUDAPPContext()
+        {
+        }
+
+        public CRUDAPPContext(DbContextOptions<CRUDAPPContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("Student");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Country)
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.RollNumber)
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
